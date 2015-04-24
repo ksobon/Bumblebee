0c695a0262bf76f63f281fe2d5aa77e8e1e7e325
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-24 18:48:23 -0400

    Create xl_ReadExcel.py
    This seems to be working well, I haven't tested for a single column which I remember was giving me trouble initially.

---
+++ xl_ReadExcel.py
@@ -0,0 +1,81 @@
+import clr
+import sys
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel
+
+System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo("en-US")
+from System.Runtime.InteropServices import Marshal
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import os.path
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+filePath = IN[0]
+runMe = IN[1]
+sheetName = IN[2]
+byColumn = IN[3]
+origin = IN[4]
+extent = IN[5]
+
+def ReadData(ws, origin, extent, byColumn):
+
+	rng = ws.Range[origin, extent].Value2
+	if not byColumn:
+		dataOut = [[] for i in range(rng.GetUpperBound(0))]
+		for i in range(rng.GetLowerBound(0)-1, rng.GetUpperBound(0), 1):
+			for j in range(rng.GetLowerBound(1)-1, rng.GetUpperBound(1), 1):
+				dataOut[i].append(rng[i,j])
+		return dataOut
+	else:
+		dataOut = [[] for i in range(rng.GetUpperBound(1))]
+		for i in range(rng.GetLowerBound(1)-1, rng.GetUpperBound(1), 1):
+			for j in range(rng.GetLowerBound(0)-1, rng.GetUpperBound(0), 1):
+				dataOut[i].append(rng[j,i])
+		return dataOut
+
+if runMe:
+	message = None
+	xlApp = Excel.ApplicationClass()
+	xlApp.Visible = False
+	xlApp.DisplayAlerts = False
+	xlApp.ScreenUpdating = False
+	if os.path.isfile(str(filePath)):
+		try:
+			xlApp.Workbooks.open(str(filePath))
+		except:
+			message = "Excel might be open. Please close it!"
+		wb = xlApp.ActiveWorkbook
+		ws = xlApp.Sheets(sheetName)
+		if origin != None:
+			origin = ws.Cells(origin[1], origin[0])
+		else:
+			origin = ws.Cells(ws.UsedRange.Row, ws.UsedRangeColumn)
+		if extent != None:
+			extent = ws.Cells(extent[1], extent[0])
+		else:
+			extent = ws.Cells(ws.UsedRange.Rows(ws.UsedRange.Rows.Count).Row, ws.UsedRange.Columns(ws.UsedRange.Columns.Count).Column)
+		dataOut = ReadData(ws, origin, extent, byColumn)
+	xlApp.ActiveWorkbook.Close(False)
+	xlApp.ScreenUpdating = True
+	Marshal.ReleaseComObject(ws)
+	Marshal.ReleaseComObject(wb)
+	Marshal.ReleaseComObject(xlApp)
+else:
+	message = "Set RunMe to True."
+
+#Assign your output to the OUT variable
+if message == None:
+	OUT = dataOut
+else:
+	OUT = message

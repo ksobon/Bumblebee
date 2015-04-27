788044de5c6278b155b7fa0e880064a8b13bb81c
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-27 17:42:18 -0400

    Create xlClearContents.py

---
+++ write/xlClearContents.py
@@ -0,0 +1,91 @@
+#Copyright(c) 2015, David Mans, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
+
+import clr
+import sys
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import os
+import os.path
+appDataPath = os.getenv('APPDATA')
+bbPath = appDataPath + r"\Dynamo\0.7\packages\Bumblebee\extra"
+if bbPath not in sys.path:
+	sys.path.Add(bbPath)
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel
+
+System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo("en-US")
+from System.Runtime.InteropServices import Marshal
+
+import bumblebee as bb
+import string
+import re
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+filePath = IN[0]
+runMe = IN[1]
+sheetName = IN[2]
+clearContent = IN[3]
+clearFormat = IN[4]
+cellRange = IN[5]
+
+def LiveStream():
+	try:
+		xlApp = Marshal.GetActiveObject("Excel.Application")
+		xlApp.Visible = True
+		xlApp.DisplayAlerts = False
+		return xlApp
+	except:
+		return None
+
+if runMe:
+	message = None
+	if LiveStream() == None:
+		xlApp = Excel.ApplicationClass()
+		xlApp.Visible = False
+		xlApp.DisplayAlerts = False
+		xlApp.ScreenUpdating = False
+		if os.path.isfile(str(filePath)):
+			xlApp.Workbooks.open(str(filePath))
+			wb = xlApp.ActiveWorkbook
+			ws = xlApp.Sheets(sheetName)
+			if cellRange != None:
+				originAddress = cellRange.split(":")[0]
+				extentAddress = cellRange.split(":")[1]
+				origin = ws.Cells(int(bb.CellIndex(originAddress)[1]), int(bb.CellIndex(originAddress)[0]))
+				extent = ws.Cells(int(bb.CellIndex(extentAddress)[1]), int(bb.CellIndex(extentAddress)[0]))
+			else:
+				origin = ws.Cells(ws.UsedRange.Row, ws.UsedRange.Column)
+				extent = ws.Cells(ws.UsedRange.Rows(ws.UsedRange.Rows.Count).Row, ws.UsedRange.Columns(ws.UsedRange.Columns.Count).Column)
+			if clearContent:
+				ws.Range[origin, extent].ClearContents()
+			if clearFormat:
+				ws.Range[origin, extent].ClearFormats()
+			wb.SaveAs(str(filePath))
+			xlApp.ActiveWorkbook.Close(False)
+			xlApp.ScreenUpdating = True
+			
+			Marshal.ReleaseComObject(extent)
+			Marshal.ReleaseComObject(origin)
+			Marshal.ReleaseComObject(ws)
+			Marshal.ReleaseComObject(wb)
+			Marshal.ReleaseComObject(xlApp)
+	else:
+		message = "Close currently running Excel \nsession."
+else:
+	message = "Run Me is set to False. Please set \nto True if you wish to write data \nto Excel."
+
+#Assign your output to the OUT variable
+if message == None:
+	OUT = "Success!"
+else:
+	OUT = '\n'.join('{:^35}'.format(s) for s in message.split('\n'))

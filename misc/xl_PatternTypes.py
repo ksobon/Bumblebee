b633d78c02d248d09da0d59e0aca6380df91ddea
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-27 17:51:01 -0400

    Create xl_PatternTypes.py

---
+++ misc/xl_PatternTypes.py
@@ -0,0 +1,11 @@
+#Copyright(c) 2015, David Mans, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+#Assign your output to the OUT variable
+OUT = ["xlCheckerBoard", "xlCrissCross", "xlDarkDiagonalDown", "xlGrey16", "xlGray25", 
+			"xlGray50", "xlGray75", "xlGray8", "xlGrid", "xlDarkHorizontal", 
+			"xlLightDiagonalDown", "xlLightHorizontal", "xlLightDiagonalUp", "xlLightVertical", "xlNone", 
+			"xlSemiGray75", "xlSolid", "xlDarkDiagonalUp", "xlDarkVertical"]

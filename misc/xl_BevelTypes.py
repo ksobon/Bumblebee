233b7879b90bc5efd67c268d945bd546d0b090c1
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-27 17:52:07 -0400

    Create xl_BevelTypes.py

---
+++ misc/xl_BevelTypes.py
@@ -0,0 +1,9 @@
+#Copyright(c) 2015, David Mans, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+#Assign your output to the OUT variable
+OUT = 	["None", "Angle", "ArtDeco", "Circle", "Convex", "CoolSlant", "Cross", "Divot", "HardEdge", "RelaxedInset", 
+		"Riblet", "Slope", "SoftRound"]

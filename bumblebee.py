550ab3145c28e7a7e4f5038d88236a4ef154c09f
Konrad K Sobon <ksobon1986@gmail.com>
2015-04-28 19:26:24 -0400

    Update bumblebee.py

--- bumblebee.py
+++ bumblebee.py
@@ -67,8 +67,7 @@ def xlRange(address):
     return [originRow, originCol, extentRow, extentCol]
 
 def GetLineStyle(key):
-    keys = ["Continuous", "Dash", "DashDot", "DashDotDot", "RoundDot", "SquareDotMSO", "LongDash", 
-            "DoubleXL", "NoneXL"]
+    keys = ["Continuous", "Dash", "DashDot", "DashDotDot", "RoundDot", "SquareDotMSO", "LongDash", "DoubleXL", "NoneXL"]
     values = [1, -4115, 4, 5, -4118, -4118, -4115, -4119, -4142]
     d = dict()
     for i in range(len(keys)):
@@ -83,10 +82,7 @@ def GetPatternType(key):
 	    "xlGray50", "xlGray75", "xlGray8", "xlGrid", "xlDarkHorizontal", 
 	    "xlLightDiagonalDown", "xlLightHorizontal", "xlLightDiagonalUp", "xlLightVertical", "xlNone", 
 	    "xlSemiGray75", "xlSolid", "xlDarkDiagonalUp", "xlDarkVertical"]
-    values = [9, 16, -4121, 17, -4124, 
-		-4124, -4126, 18, 15, -4128,
-		13, 11, 14, 12, -4142, 
-		10, 1, -4162, -4166]
+    values = [9, 16, -4121, 17, -4124, -4124, -4126, 18, 15, -4128, 13, 11, 14, 12, -4142, 10, 1, -4162, -4166]
     d = dict()
     for i in range(len(keys)):
 	    d[keys[i]] = values[i]
@@ -95,7 +91,96 @@ def GetPatternType(key):
     else:
 	    return None
 
+def GetTextHorJustType(key):
+    keys = ["Left", "Center", "Right"]
+    values = [-4131, -4108, -4152]
+    d = dict()
+    for i in range(len(keys)):
+        d[keys[i]] = values[i]
+    if key in d:
+	return d[key]
+    else:
+	return None
+
+def GetTextVerJustType(key):
+    keys = ["Bottom", "Center", "Top"]
+    values = [-4017, -4108, -4160]
+    d = dict()
+    for i in range(len(keys)):
+        d[keys[i]] = values[i]
+    if key in d:
+	return d[key]
+    else:
+	return None
+
 def RGBToRGBLong(rgb):
     strValue = '%02x%02x%02x' % rgb
     iValue = int(strValue, 16)
     return iValue
+
+def ParseFillSettings(fillStyle):
+    paramList = fillStyle.split("~")
+    patternType = GetPatternType(paramList[0])
+    # if pattern type is supplied then it needs a background color
+    # to be set so if no background color supplied it will be assigned 
+    # a default value of white
+    if paramList[1] == "xlNone" and paramList[0] == "xlNone":
+    	backColor = -4142
+    elif paramList[1] == "xlNone" and paramList[0] != "xlNone":
+    	backColor = RGBToRGBLong((255,255,255))
+    else:
+    	bColors = paramList[1].split(",")
+    	backColor = RGBToRGBLong((int(bColors[2]), int(bColors[1]), int(bColors[0])))
+    if paramList[2] == "xlNone" and paramList[0] == "xlNone":
+    	foreColor = -4142
+    elif paramList[2] == "xlNone" and paramList[0] != "xlNone":
+    	foreColor = RGBToRGBLong((0,0,0))
+    else:
+    	fColors = paramList[2].split(",")
+    	foreColor = RGBToRGBLong((int(fColors[2]), int(fColors[1]), int(fColors[0])))
+    return [patternType, backColor, foreColor]
+
+def ParseTextStyle(textStyle):
+    paramList = textStyle.split("~")
+    name = paramList[0]
+    size = int(paramList[1])
+    colorParts = paramList[2].split(",")
+    color = RGBToRGBLong((int(colorParts[2]), int(colorParts[1]), int(colorParts[0])))
+    hAlign = GetTextHorJustType(paramList[3])
+    vAlign = GetTextVerJustType(paramList[4])
+    if paramList[5] == "True":
+        bold = True
+    else:
+        bold = False
+    if paramList[6] == "True":
+        italic = True
+    else:
+        italic = False
+    if paramList[7] == "True":
+        underline = True
+    else:
+        underline = False
+    if paramList[8] == "True":
+        strikethrough = True
+    else:
+        strikethrough = False
+    return [name, size, color, hAlign, vAlign, bold, italic, underline, strikethrough]
+
+def CheckInputs(fillStyle, textStyle, borderStyle):
+    styleSettingsCheck = []
+    if fillStyle != None and any(isinstance(item, list) for item in fillStyle):
+    	styleSettingsCheck.append(True)
+    else:
+    	styleSettingsCheck.append(False)
+    if textStyle != None and any(isinstance(item, list) for item in textStyle):
+    	styleSettingsCheck.append(True)
+    else:
+    	styleSettingsCheck.append(False)
+    if borderStyle != None and any(isinstance(item, list) for item in borderStyle):
+    	styleSettingsCheck.append(True)
+    else:
+    	styleSettingsCheck.append(False)
+    if any(styleSettingsCheck):
+    	return True
+    else:
+    	return False

fe70411656dfc96d699de0a11a521975cff11aab
Konrad K Sobon <ksobon1986@gmail.com>
2015-05-16 19:11:01 -0400

    Update xl_FillStyle.py

--- styles/xl_FillStyle.py
+++ styles/xl_FillStyle.py
@@ -1,34 +1,42 @@
 #Copyright(c) 2015, David Mans, Konrad Sobon
 # @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
 
+import clr
+import sys
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+import os
+import os.path
+appDataPath = os.getenv('APPDATA')
+bbPath = appDataPath + r"\Dynamo\0.8\packages\Bumblebee\extra"
+if bbPath not in sys.path:
+	sys.path.Add(bbPath)
+
+import bumblebee as bb
+
 #The inputs to this node will be stored as a list in the IN variable.
 dataEnteringNode = IN
 
 patternType = IN[0]
-backColor = IN[1]
-foreColor = IN[2]
+backgroundColor = IN[1]
+patternColor = IN[2]
 opacity = IN[3]
 bevelType = IN[4]
 
-if patternType == None:
-	patternType = "xlNone"
-if opacity == None:
-	opacity = 0
-if bevelType == None:
-	bevelType = "None"
-if backColor != None:
-	bcolor = ",".join([str(backColor.Red), str(backColor.Green), str(backColor.Blue)])
-else:
-	bcolor = "xlNone"
-if foreColor != None:
-	fcolor = ",".join([str(foreColor.Red), str(foreColor.Green), str(foreColor.Blue)])
-	if backColor == None:
-		bcolor = ",".join([str(255), str(255), str(255)])
-	else:
-		bcolor = ",".join([str(backColor.Red), str(backColor.Green), str(backColor.Blue)])
-else:
-	fcolor = "xlNone"
-cellFill = "~".join([patternType, bcolor, fcolor, str(opacity), bevelType])
+fillStyle = bb.BBFillStyle()
+
+if patternType != None:
+	fillStyle.patternType = patternType
+if backgroundColor != None:
+	fillStyle.backgroundColor = backgroundColor
+if patternColor != None:
+	fillStyle.patternColor = patternColor
+if opacity != None:
+	fillStyle.opacity = opacity
+if bevelType != None:
+	fillStyle.bevelType = bevelType
 
 #Assign your output to the OUT variable
-OUT = cellFill
+OUT = fillStyle

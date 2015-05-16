f2b1a573addb882f30091f2b8c9dbb17b5833f28
Konrad K Sobon <ksobon1986@gmail.com>
2015-05-16 19:13:49 -0400

    Create xl_OperatorTypes.py

---
+++ types/xl_OperatorTypes.py
@@ -0,0 +1,8 @@
+#Copyright(c) 2015, David Mans, Konrad Sobon
+# @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
+
+#The inputs to this node will be stored as a list in the IN variable.
+dataEnteringNode = IN
+
+#Assign your output to the OUT variable
+OUT = 	["Equal", "NotEqual", "Greater", "GreaterEqual", "Less", "LessEqual", "Between", "NotBetween"]

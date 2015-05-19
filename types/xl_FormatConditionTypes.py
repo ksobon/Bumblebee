9f680a957573771d544fc91968e0e02f4b593cf6
Konrad K Sobon <ksobon1986@gmail.com>
2015-05-19 19:01:10 -0400

    Delete xl_FormatConditionTypes.py

--- types/xl_FormatConditionTypes.py
+++
@@ -1,8 +0,0 @@
-#Copyright(c) 2015, David Mans, Konrad Sobon
-# @arch_laboratory, http://archi-lab.net, http://neoarchaic.net
-
-#The inputs to this node will be stored as a list in the IN variable.
-dataEnteringNode = IN
-
-#Assign your output to the OUT variable
-OUT = 	["CellValue", "Expression"]
